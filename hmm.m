3c017f7f3e1ead22c651f327919b883db8347b3c
Mohammad <m.azadeh@ut.ac.ir>
2017-01-02 10:59:48 +0330

    HMM Initialization Finished
    Next step is to restimate.

--- hmm.m
+++ hmm.m
@@ -1,67 +1,76 @@
 function output = hmm(name)
 clc;
-
 global PATH_SEPARATOR
-% read sample directory
+
+%% read samples from train directory
+% creating sample list
 filename = strcat('data', PATH_SEPARATOR, 'train', PATH_SEPARATOR, name);
 sample_list = dir(filename);
 sample_list = sample_list(3:end);
-sample_list_size = size(sample_list, 1);
+sample_counts = size(sample_list, 1);
+state_counts = 8;
 
-% read sample ext files
-sample_data(sample_list_size) = struct('name', [], 'feature_vector', [], 'vector_dimension', [], 'data', []);
-for i = 1 : sample_list_size
+%creating empty model
+samples(sample_counts) = struct('feature_counts', [], 'features_dimension', [], 'observations', []);
+states(state_counts) = struct('cluster_counts', [], 'features', []);
+model = struct('name', name, 'sample_counts', sample_counts, 'samples', samples, 'state_counts', state_counts, 'states', states);
+
+% read samples (ext files)
+for i = 1 : sample_counts
     sample_path = strcat(sample_list(i).folder, PATH_SEPARATOR, sample_list(i).name);
     sample_file = fopen(sample_path);
-    sample_data(i).name = sample_list(i).name;
-    sample_data(i).feature_vector = fscanf(sample_file, '%d', 1);
-    sample_data(i).vector_dimension = fscanf(sample_file, '%d', 1);
-    sample_data(i).data = zeros(sample_data(i).feature_vector, sample_data(i).vector_dimension);
-    for j = 1 : sample_data(i).feature_vector
-        sample_data(i).data(j, 1:end) = fscanf(sample_file, '%f', sample_data(i).vector_dimension);
+    model.samples(i).name = sample_list(i).name;
+    model.samples(i).feature_counts = fscanf(sample_file, '%d', 1);
+    model.samples(i).features_dimension = fscanf(sample_file, '%d', 1);
+    model.samples(i).observations = zeros(model.samples(i).feature_counts, model.samples(i).features_dimension);
+    for j = 1 : model.samples(i).feature_counts
+        model.samples(i).observations(j, 1:end) = fscanf(sample_file, '%f', model.samples(i).features_dimension);
     end
-    % disp(sample_data(i));
     fclose(sample_file);
 end
 
-% hmm initialization
-hmm_state_length = 8;
-hmm_a = zeros(hmm_state_length);
-for i = 1 : hmm_state_length
-    hmm_a(i, i) = 0.5;
-    hmm_a(i, i+1) = 0.5;
-end
-hmm_a(hmm_state_length, hmm_state_length) = 0.5;
-hmm_state_gaussians = 3;
-hmm_gaussian_weights = zeros(hmm_state_length*hmm_state_gaussians);
-hmm_gaussians(hmm_state_length) = struct('mean', [], 'variance', []);
-
-for i = hmm_state_length : -1 : 1
-    hmm_state_features(i) = struct('features', [], 'clusters', []);
+%% hmm model initialization
+for i = 1 : model.state_counts
+    model.states(i).cluster_counts = 3;
+    for j = 1 : model.states(i).cluster_counts
+        model.states(i).clusters(j) = struct('gaussian_mean', [], 'gaussian_variance', [], 'gaussian_weight', [], 'features', []);
+    end
 end
 
-for i = 1 : sample_list_size
-    state_features_count = floor((sample_data(i).feature_vector)/hmm_state_length);
-    for j = 1 : hmm_state_length
-        first_index = (j-1)*state_features_count + 1;
-        last_index = j*state_features_count;
-        if (j == hmm_state_length)
-            last_index = sample_data(i).feature_vector;
+%%% initial clustering
+% divide observations between states
+for i = 1 : sample_counts
+    state_feature_counts = floor((model.samples(i).feature_counts)/model.state_counts);
+    for j = 1 : model.state_counts
+        first_index = (j-1)*state_feature_counts + 1;
+        last_index = j*state_feature_counts;
+        if (j == model.state_counts)
+            last_index = model.samples(i).feature_counts;
         end
-        if (i == 1)
-            hmm_state_features(j).features = sample_data(i).data(first_index:last_index,:);
+        if i == 1
+            model.states(j).features = model.samples(i).observations(first_index:last_index,:);
         else
-            hmm_state_features(j).features = cat(1, hmm_state_features(j).features, sample_data(i).data(first_index:last_index,:));
+            model.states(j).features = cat(1, model.states(j).features, model.samples(i).observations(first_index:last_index,:));
         end
     end
 end
-% disp(size(hmm_state_features(1).features));
-% disp(size(hmm_state_features(2).features));
-% disp(size(hmm_state_features(3).features));
-% disp(size(hmm_state_features(4).features));
-% disp(size(hmm_state_features(5).features));
-% disp(size(hmm_state_features(6).features));
-% disp(size(hmm_state_features(7).features));
-% disp(size(hmm_state_features(8).features));
+
+% clustering
+for i = 1 : model.state_counts
+    model.states(i) = hmm_clustering(model.states(i));
+end
+
+%%% initial matrix a
+model.a = zeros(model.state_counts);
+for i = 1 : model.state_counts - 1
+    model.a(i, i) = 0.5;
+    model.a(i, i+1) = 0.5;
+end
+model.a(model.state_counts, model.state_counts) = 0.5;
+
+disp(hmm_b(model.states(1), model.samples(1).observations(1,:)));
+
+%% Restimation
+
 
 end
